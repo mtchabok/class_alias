9549d50aa64ffbba1ec7ba7ea69a6958b4fa177e
Mohammad Taghi Chabok <mtchabok@users.noreply.github.com>
2020-04-08 23:16:51 +0430

    Update ClassAlias.php

--- src/ClassAlias.php
+++ src/ClassAlias.php
@@ -1,55 +1,72 @@
 <?php
 namespace Mtchabok\ClassAlias;
 
+use ArrayAccess;
+
 /**
  * Class ClassAlias
  * @package Mtchabok\ClassAlias
+ *
+ * @property-read string name
+ * @property null|ClassAlias parent
  */
-class ClassAlias
+class ClassAlias implements ArrayAccess
 {
 	/** @var string */
 	protected $_name = '';
 
-	/** @var array[] */
+	/** @var string|ClassAlias */
+	protected $_parent = '';
+
+	/** @var ClassAliasDetails[] */
 	protected $_aliasDetails = [];
 
-	/** @var callable[] */
-	protected $_onAdd = [];
 
 
 	/** @var ClassAlias[] */
-	protected static $_instances = [];
+	private static $_instances = [];
+	private static $_instancesHash = [];
+
+
 
 
 	/**
-	 * @param string|array $alias
+	 * @param string|array|ClassAliasDetails|ClassAliasDetails[] $alias
 	 * 		<br>alias name string
+	 * 		<br>new ClassAliasDetails(array('alias'=>'alias name', 'className'=>'class name', ...))
 	 * 		<br>array('alias'=>'alias name', 'className'=>'class name', ...)
 	 * 		<br>array( array('alias'=>'alias name', 'className'=>'class name', ...), array('alias'=>'alias name', 'className'=>'class name', ...) )
 	 * @param string $className [optional]
 	 * @param array $details [optional]
+	 * @param bool $mergeOnExist [optional]
 	 * @return ClassAlias
 	 */
-	public function add(string $alias, string $className = null, array $details = null) :ClassAlias
+	public function add($alias, string $className = null, array $details = null, bool $mergeOnExist = true) :ClassAlias
 	{
 		$aliases = [];
-		if(is_array($alias) && !empty($alias['alias']))
+		if(is_array($alias)) {
+			$aliases = !empty($alias['alias']) ? [$alias] : $alias;
+		}elseif ($alias instanceof ClassAliasDetails)
 			$aliases = [$alias];
-		elseif (is_array($alias) && !empty($alias[0]['alias']))
-			$aliases = $alias;
-		elseif (is_string($alias) && $alias)
+		elseif (is_string($alias))
 			$aliases = [array_merge(
 				is_array($details) ?$details :[]
 				, ['alias'=>(string) $alias, 'className'=>(string) $className]
 			)];
 		unset($alias, $className, $details);
+		if(!$ClassAliasDetailsCN = $this->getClassName('MtchabokClassAliasDetails')) {
+			if (''===$this->_name || !$ClassAliasDetailsCN = static::getClassAlias()->getClassName('MtchabokClassAliasDetails'))
+				$ClassAliasDetailsCN = ClassAliasDetails::class;
+		}
 		while ($aliasDetails = array_shift($aliases)){
-			foreach ($this->_onAdd as $func){
-				if(is_array($result = call_user_func($func, $aliasDetails)) && $result)
-					$aliasDetails = $result;
+			if((is_array($aliasDetails) || $aliasDetails instanceof ClassAliasDetails) && !empty($aliasDetails['alias'])){
+				if($mergeOnExist && array_key_exists($aliasDetails['alias'], $this->_aliasDetails))
+					$this->_aliasDetails[$aliasDetails['alias']]->merge($aliasDetails);
+				elseif($aliasDetails instanceOf ClassAliasDetails)
+					$this->_aliasDetails[$aliasDetails['alias']] = $aliasDetails;
+				else
+					$this->_aliasDetails[$aliasDetails['alias']] = new $ClassAliasDetailsCN($aliasDetails);
 			}
-			if(!empty($aliasDetails['alias']))
-				$this->_aliasDetails[$aliasDetails['alias']] = $aliasDetails;
 		}
 		return $this;
 	}
@@ -59,17 +76,18 @@ class ClassAlias
 	 * @return bool
 	 */
 	public function exist(string $alias)
-	{ return isset($this->_aliasDetails[$alias]); }
+	{ return isset($this->_aliasDetails[$alias]) || ( ($CA=$this->parent) && $CA->exist($alias)); }
 
 	/**
 	 * @param callable $matchFunc
-	 * 		<br>function(array $aliasDetails) :bool {
+	 * 		<br>function(ClassAliasDetails $aliasDetails) :bool {
 	 * 			<br>>if( is match $aliasDetails ) return true; else return false;
 	 * 		<br>}
 	 * @param bool $revers [optional]
+	 * @param bool $localWork [optional]
 	 * @return string[]
 	 */
-	public function find(callable $matchFunc, bool $revers = false) :array
+	public function find(callable $matchFunc, bool $revers = null, $localWork = null) :array
 	{
 		$foundAliases = [];
 		$aliases = array_keys($this->_aliasDetails);
@@ -77,43 +95,47 @@ class ClassAlias
 			if(call_user_func($matchFunc, $this->_aliasDetails[$alias]))
 				$foundAliases[] = $alias;
 		}
+		if(!$localWork && ($CA = $this->parent))
+			$foundAliases+= $CA->find($matchFunc, $revers);
 		return $foundAliases;
 	}
 
 	/**
 	 * @param string $alias
-	 * @return array|null
+	 * @return ClassAliasDetails|null
 	 */
 	public function get(string $alias)
-	{ return $this->exist($alias) ?$this->_aliasDetails[$alias] :null; }
+	{
+		if(isset($this->_aliasDetails[$alias]))
+			return $this->_aliasDetails[$alias];
+		elseif ($CA = $this->parent)
+			return $CA->get($alias);
+		return null;
+	}
 
 	/**
 	 * @param string $alias
 	 * @return string
 	 */
 	public function getClassName(string $alias) :string
-	{ return !empty($this->_aliasDetails[$alias]['className']) ?(string) $this->_aliasDetails[$alias]['className'] :''; }
-
-
-
-
-	/**
-	 * @param callable $func
-	 * 		function(array $aliasDetails) :array {
-	 * 			add anything to details : $aliasDetails['group'] = '';
-	 * 			return $aliasDetails
-	 * 		}
-	 * @param bool $prepend [optional]
-	 * @return ClassAlias
-	 */
-	public function onAdd(callable $func, $prepend = false) :ClassAlias
 	{
-		if(!in_array($func, $this->_onAdd)){
-			if($prepend) array_unshift($this->_onAdd, $func);
-			else array_push($this->_onAdd, $func);
-		}
-		return $this;
+		$detail = $this->get($alias);
+		return !empty($detail['className']) ?(string) $detail['className'] :'';
 	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -121,13 +143,66 @@ class ClassAlias
 
 	/**
 	 * @param string $name
-	 * @return ClassAlias
+	 * @return bool
 	 */
-	public static function getClassAlias(string $name) :ClassAlias
+	public static function existClassAlias(string $name) :bool
+	{ return empty($name) || isset(self::$_instances[(string) $name]); }
+
+	/**
+	 * @param string $name
+	 * @return ClassAlias|null
+	 */
+	public static function getClassAlias(string $name = null)
 	{
-		if(!isset(self::$_instances[$name]))
-			self::$_instances[$name] = new static($name);
-		return self::$_instances[$name];
+		if(empty($name) && !isset(static::$_instances[''])){
+			$CA = new static();
+			$CA->_name = '';
+			$CA->_parent = null;
+			self::$_instances[''] = $CA;
+			self::$_instancesHash[''] = spl_object_hash($CA);
+			$CA->add([
+				['alias'=>'MtchabokClassAlias', 'className'=>ClassAlias::class],
+				['alias'=>'MtchabokClassAliasDetails', 'className'=>ClassAliasDetails::class],
+			]);
+		}
+		return array_key_exists((string)$name, self::$_instances) ?self::$_instances[(string)$name] :null;
+	}
+
+	/**
+	 * @param ClassAlias $classAlias
+	 */
+	public static function addClassAlias(ClassAlias $classAlias)
+	{
+		if(!in_array(spl_object_hash($classAlias), self::$_instancesHash)) {
+			if (empty($classAlias->_name) || !is_string($classAlias->_name))
+				$classAlias->_name = uniqid('CLASS_ALIAS_');
+			if (assert(!isset(self::$_instances[$classAlias->_name]))) {
+				self::$_instances[$classAlias->_name] = $classAlias;
+				self::$_instancesHash[$classAlias->_name] = spl_object_hash($classAlias);
+			}
+		}
+	}
+
+	/**
+	 * @param array|string $properties [optional]
+	 * @return ClassAlias|null
+	 */
+	public static function newClassAlias($properties = null)
+	{
+		if(!is_array($properties)) $properties = ['name'=>(string) $properties];
+		else $properties+=['name'=>null];
+
+		$className = static::class;
+		/** @var ClassAlias $obj */
+		if(!assert(($obj = new $className()) instanceof ClassAlias))
+			return null;
+
+		$obj->_name = !empty($properties['name']) && is_string($properties['name']) ?$properties['name'] :uniqid('CLASS_ALIAS_');
+
+		if(isset($properties['parent']))
+			$obj->parent = $properties['parent'];
+
+		return $obj;
 	}
 
 
@@ -136,8 +211,27 @@ class ClassAlias
 
 
 
-	private function __construct(string $name)
-	{ $this->_name = $name; }
+
+
+	final private function __construct()
+	{}
+
+	public function offsetExists($offset)
+	{ return $this->exist($offset); }
+
+	public function offsetGet($offset)
+	{ return $this->get($offset); }
+
+	public function offsetSet($offset, $value)
+	{
+		if(is_array($value))
+			$this->add(array_merge($value, ['alias'=>$offset]));
+		elseif (is_string($value))
+			$this->add($offset, $value);
+	}
+
+	public function offsetUnset($offset)
+	{}
 
 	public function __call($name, $arguments)
 	{
@@ -149,26 +243,51 @@ class ClassAlias
 		return null;
 	}
 
-	public function __isset($name)
-	{ return $this->exist($name); }
-
 	public function __get($name)
-	{ return $this->get($name); }
+	{
+		switch ($name){
+			case 'name': return $this->_name; break;
+			case 'parent':
+				if(is_string($this->_parent) && $this->_parent!=$this->_name)
+					return static::getClassAlias($this->_parent);
+				elseif ($this->_parent instanceof ClassAlias)
+					return $this->_parent;
+				else
+					return null;
+				break;
+		}
+		return null;
+	}
 
 	public function __set($name, $value)
-	{}
+	{
+		switch ($name){
+			case 'parent':
+				if (!empty($this->_name)){
+					if(false===$value || is_null($value))
+						$this->_parent = null;
+					elseif ($value instanceof ClassAlias && $this->_name!=$value->_name)
+						$this->_parent = static::existClassAlias($value) ?$value->_name :$value;
+					elseif (is_string($value) && $this->_name!=$value && static::existClassAlias($value))
+						$this->_parent = $value;
+				}
+				break;
+		}
+	}
+
+	public function __isset($name)
+	{
+		switch ($name){
+			case 'name': return ''===$this->_name || !empty($this->_name); break;
+			case 'parent': return ''===$this->_parent || !empty($this->_parent); break;
+		} return false;
+	}
 
 	public function __unset($name)
 	{}
 
-	public function __sleep()
-	{ return ['_name', '_aliasDetails']; }
 
-	public function __wakeup()
-	{}
-
-
-	public function __toString()
+	final public function __toString()
 	{ return $this->_name; }
 
 }
