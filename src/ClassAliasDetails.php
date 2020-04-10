f56931d20a8a0c4c0c7aee9a2ea63616330a6041
Mohammad Taghi Chabok <mtchabok@users.noreply.github.com>
2020-04-10 11:18:17 +0430

    Update ClassAliasDetails.php
    add "exist, get, set, delete" method`s
    add magic method

--- src/ClassAliasDetails.php
+++ src/ClassAliasDetails.php
@@ -5,14 +5,66 @@ namespace Mtchabok\ClassAlias;
  * Class ClassAliasDetails
  * @package Mtchabok\ClassAlias
  *
- * @property-read string alias
- * @property string className
- * @property string|array link
+ * @property-read 	string 				alias
+ * @property 		string 				className
+ * @property 		string|array 		link
+ *
+ * @method 			string 				getAlias()
+ *
+ * @method 			string 				getClassName( string $default = null )
+ * @method 			ClassAliasDetails 	setClassName( string $className )
+ *
+ * @method 			bool 				existLink()
+ * @method 			string 				getLink( string|array $default = null )
+ * @method 			ClassAliasDetails 	setLink( string|array $link )
+ * @method 			ClassAliasDetails 	deleteLink()
  */
 class ClassAliasDetails implements \ArrayAccess
 {
 	/** @var mixed[] */
 	protected $_details = [];
+
+
+
+
+
+	/**
+	 * @param string $index
+	 * @return bool
+	 */
+	public function exist(string $index) :bool
+	{ return array_key_exists($index, $this->_details); }
+
+	/**
+	 * @param string $index
+	 * @param mixed $default
+	 * @return mixed
+	 */
+	public function get(string $index, $default = null)
+	{ return array_key_exists($index, $this->_details) ?$this->_details[$index] :$default; }
+
+	/**
+	 * @param string $index
+	 * @param mixed $value
+	 * @return $this
+	 */
+	public function set(string $index, $value)
+	{ if(!in_array($index, ['alias'])) $this->_details[$index] = $value; return $this; }
+
+	/**
+	 * @param string $index
+	 * @return $this
+	 */
+	public function delete(string $index)
+	{
+		if(in_array($index, ['link', 'className'])) {
+			if (array_key_exists($index, $this->_details)) $this->_details[$index] = '';
+		}elseif(!in_array($index, ['alias'])) unset($this->_details[$index]);
+		return $this;
+	}
+
+
+
 
 	/**
 	 * @param array|ClassAliasDetails $details
@@ -37,6 +89,8 @@ class ClassAliasDetails implements \ArrayAccess
 	{ return $this->_details; }
 
 
+
+
 	public function __construct(array $details = null)
 	{
 		if(is_array($details))
@@ -47,39 +101,45 @@ class ClassAliasDetails implements \ArrayAccess
 			$this->_details['className'] = '';
 	}
 
+	public function __call($name, $arguments)
+	{
+		if(preg_match('#^(get|set|exist|delete)(.*)$#', $name, $parsed)){
+			$index = strtolower(substr($parsed[2], 0, 1)) . substr($parsed[2], 1);
+			switch ($parsed[1]){
+				case 'exist': return $this->exist($index); break;
+				case 'get': return $this->get($index, isset($arguments[0]) ?$arguments[0] :null); break;
+				case 'set': return $this->set($index, $arguments[0]); break;
+				case 'delete': return $this->delete($index);
+			}
+		}
+		return null;
+	}
+
 
 	public function offsetExists($offset)
-	{ return array_key_exists($offset, $this->_details); }
+	{ return $this->exist($offset); }
 
 	public function offsetGet($offset)
-	{ return array_key_exists($offset, $this->_details) ?$this->_details[$offset] :null; }
+	{ return $this->get($offset); }
 
 	public function offsetSet($offset, $value)
-	{ if(!in_array($offset, ['alias'])) $this->_details[$offset] = $value; }
+	{ $this->set($offset, $value); }
 
 	public function offsetUnset($offset)
-	{
-		if(in_array($offset, ['link', 'className'])) {
-			if (isset($this->_details[$offset])) $this->_details[$offset] = '';
-		}elseif(!in_array($offset, ['alias'])) unset($this->_details[$offset]);
-	}
+	{ $this->delete($offset); }
 
-
-	public function __get($name)
-	{ return array_key_exists($name, $this->_details) ?$this->_details[$name] :null; }
-
-	public function __set($name, $value)
-	{ if(!in_array($name, ['alias'])) $this->_details[$name] = $value; }
 
 	public function __isset($name)
-	{ return array_key_exists($name, $this->_details); }
+	{ return $this->exist($name); }
+
+	public function __get($name)
+	{ return $this->get($name); }
+
+	public function __set($name, $value)
+	{ $this->set($name, $value); }
 
 	public function __unset($name)
-	{
-		if(in_array($name, ['link', 'className'])) {
-			if (isset($this->_details[$name])) $this->_details[$name] = '';
-		}elseif(!in_array($name, ['alias'])) unset($this->_details[$name]);
-	}
+	{ $this->delete($name); }
 
 
 	public function __toString()
